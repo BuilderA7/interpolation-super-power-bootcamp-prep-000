262546cf7d8d45c3aeec8a8db8de8d4de2667cf9
A I <atbisrael@gmail.com>
2018-01-03 03:29:26 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  R = colors[0]
+  O = colors[1]
+  Y = colors[2]
+  G = colors[3]
+  B = colors[4]
+  I = colors[5]
+  V = colors[6]
+  puts "R: #{R}, O: #{O}, Y: #{Y}, G: #{G}, B: #{B}, I: #{I}, V: #{V}"
+end
